adb9f0016dd125cd0f7d5f464a3601091bdd0237
GrowlyX <growlyxdev@gmail.com>
2022-03-12 16:33:09 -0500

    Initial commit

---
+++ src/main/kotlin/design/lmao/shuffle/WoolShuffle.kt
@@ -0,0 +1,15 @@
+package design.lmao.shuffle
+
+import org.bukkit.plugin.java.JavaPlugin
+
+/**
+ * @author GrowlyX
+ * @since 3/12/2022
+ */
+class WoolShuffle : JavaPlugin()
+{
+    override fun onEnable()
+    {
+
+    }
+}
