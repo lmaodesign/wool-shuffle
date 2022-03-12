7b2de27553a77854dc51bc811eee3728dcec2daa
GrowlyX <growlyxdev@gmail.com>
2022-03-12 16:46:02 -0500

    Flavor

---
+++ src/main/kotlin/design/lmao/shuffle/WoolShuffleHandler.kt
@@ -0,0 +1,18 @@
+package design.lmao.shuffle
+
+import gg.scala.flavor.service.Configure
+import gg.scala.flavor.service.Service
+
+/**
+ * @author GrowlyX
+ * @since 3/12/2022
+ */
+@Service
+object WoolShuffleHandler
+{
+    @Configure
+    fun configure()
+    {
+
+    }
+}
