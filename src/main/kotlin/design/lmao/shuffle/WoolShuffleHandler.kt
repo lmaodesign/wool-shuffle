ab4b8aec85fa191890f26e2a269ef62de525893b
devrawr <devrawr@protonmail.com>
2022-03-12 23:24:45 +0100

    add Listener.kt

--- src/main/kotlin/design/lmao/shuffle/WoolShuffleHandler.kt
+++ src/main/kotlin/design/lmao/shuffle/WoolShuffleHandler.kt
@@ -1,8 +1,12 @@
 package design.lmao.shuffle
 
+import design.lmao.shuffle.util.Listener
 import gg.scala.flavor.inject.Inject
 import gg.scala.flavor.service.Configure
 import gg.scala.flavor.service.Service
+import org.bukkit.Bukkit
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.metadata.FixedMetadataValue
 
 /**
  * @author GrowlyX
@@ -17,5 +21,27 @@ object WoolShuffleHandler
     @Configure
     fun configure()
     {
+        Listener
+            .listenTo<PlayerJoinEvent>()
+            .apply(plugin)
+            .on {
+                val player = it.player
+
+                // TODO: 3/12/22 check if match has been started
+
+                if (plugin.config.maxPlayers >= Bukkit.getOnlinePlayers().size + 1
+                    && !player.hasPermission("shuffle.bypass")
+                )
+                {
+                    player.setMetadata(
+                        "spectator",
+                        FixedMetadataValue(
+                            plugin,
+                            ""
+                        )
+                    )
+                    // TODO: 3/12/22 set player to spectator
+                }
+            }
     }
 }
---
+++ src/main/kotlin/design/lmao/shuffle/util/Listener.kt
@@ -0,0 +1,92 @@
+package design.lmao.shuffle.util
+
+import org.bukkit.event.Cancellable
+import org.bukkit.event.Event
+import org.bukkit.event.EventPriority
+import org.bukkit.plugin.java.JavaPlugin
+
+object Listener
+{
+    inline fun <reified T : Event> listenTo() : ListenerBuilder<T>
+    {
+        return ListenerBuilder(T::class.java)
+    }
+}
+
+class ListenerBuilder<T : Event>(val type: Class<T>)
+{
+    val filters = mutableListOf<Filter<T>>()
+    val cancelOn = mutableListOf<Filter<T>>()
+    val handle = mutableListOf<HandleEvent<T>>()
+
+    var priority: EventPriority = EventPriority.NORMAL
+
+    fun filter(filter: Filter<T>): ListenerBuilder<T>
+    {
+        return this.apply {
+            this.filters.add(filter)
+        }
+    }
+
+    fun cancelOn(filter: Filter<T>): ListenerBuilder<T>
+    {
+        return this.apply {
+            this.cancelOn.add(filter)
+        }
+    }
+
+    fun priority(priority: EventPriority): ListenerBuilder<T>
+    {
+        return this.apply {
+            this.priority = priority
+        }
+    }
+
+    fun on(handle: HandleEvent<T>): ListenerBuilder<T>
+    {
+        return this.apply {
+            this.handle.add(handle)
+        }
+    }
+
+    fun apply(plugin: JavaPlugin): ListenerBuilder<T>
+    {
+        return this.apply {
+            val pluginManager = plugin.server.pluginManager
+            val listener = object : org.bukkit.event.Listener
+            {}
+
+            pluginManager.registerEvent(
+                type,
+                listener,
+                priority,
+                { _, event ->
+                    for (filter in filters)
+                    {
+                        if (!filter.invoke(event as T))
+                        {
+                            return@registerEvent
+                        }
+                    }
+
+                    for (function in cancelOn)
+                    {
+                        if (function.invoke(event as T) && event is Cancellable)
+                        {
+                            event.isCancelled = true
+                        }
+                    }
+
+                    for (function in handle)
+                    {
+                        function.invoke(event as T)
+                    }
+                },
+                plugin
+            )
+        }
+    }
+}
+
+typealias Filter<T> = (T) -> Boolean
+typealias HandleEvent<T> = (T) -> Unit
